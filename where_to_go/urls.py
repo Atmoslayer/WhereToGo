784319b804f26ec4213c89d1a934d5ba37eea451
Atmoslayer <ddsomdim@gmail.com>
2023-03-24 04:02:26 +0300

    Setup media files serving

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from map_poster import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.show_main)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
