9dde595ea56794656fad03bedba106662c9dc809
Atmoslayer <ddsomdim@gmail.com>
2023-04-04 22:13:30 +0300

    Move coordinates to Place model

--- map_poster/models.py
+++ map_poster/models.py
@@ -5,23 +5,11 @@ class Place(models.Model):
     title = models.CharField('Название', max_length=50)
     description_short = models.CharField('Короткое описание', max_length=500)
     description_long = models.TextField('Длинное описание')
-
-    def __str__(self):
-        return self.title
-
-
-class Coordinates(models.Model):
-    place = models.ForeignKey(
-        'Place',
-        on_delete=models.CASCADE,
-        verbose_name='Место',
-        related_name='coordinates'
-    )
     lat = models.FloatField(max_length=10, verbose_name='Широта')
     lon = models.FloatField(max_length=10, verbose_name='Долгота')
 
     def __str__(self):
-        return self.place.title
+        return self.title
 
 
 class Image(models.Model):
