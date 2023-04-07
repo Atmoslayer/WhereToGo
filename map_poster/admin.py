336e660a137ad032a89cb80d82ea2141e7987f47
Atmoslayer <ddsomdim@gmail.com>
2023-04-07 09:29:04 +0300

    Add images thumbnails in admin panel

--- map_poster/admin.py
+++ map_poster/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from django.utils.html import format_html
+
 from .models import Place, Image
 
 
 class ImageInline(admin.StackedInline):
     model = Image
     extra = 0
+
+    readonly_fields = ['place_images']
+
+    def place_images(self, object):
+        return format_html(f'<img src="{object.image.url}" height=200px />')
+
+    place_images.short_description = 'Миниатюра'
 
 
 @admin.register(Place)
