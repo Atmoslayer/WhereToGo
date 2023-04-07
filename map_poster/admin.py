aebee363ea7543b8fb4d0acdbf3a12fd1d8b8578
Atmoslayer <ddsomdim@gmail.com>
2023-04-07 09:29:54 +0300

    Make inline tabular

--- map_poster/admin.py
+++ map_poster/admin.py
@@ -4,7 +4,7 @@ from django.utils.html import format_html
 from .models import Place, Image
 
 
-class ImageInline(admin.StackedInline):
+class ImageInline(admin.TabularInline):
     model = Image
     extra = 0
 
