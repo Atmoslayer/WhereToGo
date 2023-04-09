78a9ac793486e671446417baa21804864a9fef68
Atmoslayer <ddsomdim@gmail.com>
2023-04-09 22:11:19 +0300

    Change staticfiles dirs settings

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -113,9 +113,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+if not DEBUG:
+    STATIC_ROOT = ''
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-    ]
+    os.path.join(BASE_DIR, 'static/'),
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
