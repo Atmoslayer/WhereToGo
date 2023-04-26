69c656c3885f8a2e35513c54d09aec2fc76eb6e1
Atmoslayer <ddsomdim@gmail.com>
2023-04-26 22:08:34 +0300

    Fix allowed hosts setting

--- where_to_go/settings.py
+++ where_to_go/settings.py
@@ -17,8 +17,9 @@ SECRET_KEY = env.str('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.str('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', '127.0.0.1')
 
+print(ALLOWED_HOSTS)
 
 # Application definition
 
